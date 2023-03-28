314dc3f0580a98ecaa1642cfca92b4a49f4521fc
parasgroot2005 <44232270+parasgroot2005@users.noreply.github.com>
2023-03-28 17:27:11 +0530

    added session auth as well

--- task/views.py
+++ task/views.py
@@ -2,14 +2,14 @@ from .models import Task, Blocker
 from .serializers import TaskSerializer,BlockerSerializer
 from rest_framework import viewsets
 from datetime import datetime, timedelta
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication], SessionAuthentication
     permission_classes = [IsAuthenticated]
 
 class ReportViewSet(viewsets.ReadOnlyModelViewSet):
@@ -17,12 +17,12 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
     to_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
     queryset = Task.objects.filter(working_date__range=[from_date, to_date])
     serializer_class = TaskSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
 
 class BlockerViewSet(viewsets.ModelViewSet):    
     queryset = Blocker.objects.all()
     serializer_class = BlockerSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
 
